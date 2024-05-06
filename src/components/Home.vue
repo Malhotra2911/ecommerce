7f9807000d1406da98128a85c1f681edfe776758
Lucky <lokendramalhotra2001@gmail.com>
2024-05-06 16:21:07 +0530

    /public added on image route

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -38,7 +38,7 @@ export default {
         <section class="displayBanner">
             <div class="row displayBanner1">
                 <div class="col-md-5 displayBanner1_1">
-                    <img src="../../landingPage/10.png" alt="" class="fit-image">
+                    <img src="../../public/landingPage/10.png" alt="" class="fit-image">
                 </div>
                 <div class="col-md-7 text-uppercase displayBanner1_2">
                     <p> <span class="d-flex justify-content-center displayBanner1_21">Online Business <br> and E-commerce</span><span class="d-flex justify-content-evenly displayBanner1_22">Lorem ipsum dolor sit amet.</span></p>
@@ -56,21 +56,21 @@ export default {
                         </div>
                         <div class="carousel-inner">
                             <div class="carousel-item active">
-                            <img src="../../landingPage/1.jpg" class="d-block w-100" alt="...">
+                            <img src="../../public/landingPage/1.jpg" class="d-block w-100" alt="...">
                             <div class="carousel-caption d-none d-md-block">
                                 <h5>First slide label</h5>
                                 <p>Some representative placeholder content for the first slide.</p>
                             </div>
                             </div>
                             <div class="carousel-item">
-                            <img src="../../landingPage/1.jpg" class="d-block w-100" alt="...">
+                            <img src="../../public/landingPage/1.jpg" class="d-block w-100" alt="...">
                             <div class="carousel-caption d-none d-md-block">
                                 <h5>Second slide label</h5>
                                 <p>Some representative placeholder content for the second slide.</p>
                             </div>
                             </div>
                             <div class="carousel-item">
-                            <img src="../../landingPage/1.jpg" class="d-block w-100" alt="...">
+                            <img src="../../public/landingPage/1.jpg" class="d-block w-100" alt="...">
                             <div class="carousel-caption d-none d-md-block">
                                 <h5>Third slide label</h5>
                                 <p>Some representative placeholder content for the third slide.</p>
